74027ff7341923275c12914cc5249832f95dba1e
Baptiste Langlade <baptouuuu@gmail.com>
2023-11-15 18:41:52 +0100

    add responses headers

--- src/Response/Definition.php
+++ src/Response/Definition.php
@@ -17,24 +17,31 @@ use Innmind\OpenAPI\Type\{
     Number,
 };
 use Innmind\MediaType\MediaType;
+use Innmind\Immutable\Map;
 
 final class Definition
 {
     private MediaType $mediaType;
     private Shape|Sequence|Str|Uuid|Password|Url|Date|DateTime|File|Integer|Number $content;
     private ?string $description;
+    /** @var Map<non-empty-string, Str> */
+    private Map $headers;
 
     /**
      * @psalm-mutation-free
+     *
+     * @param Map<non-empty-string, Str> $headers
      */
     private function __construct(
         MediaType $mediaType,
         Shape|Sequence|Str|Uuid|Password|Url|Date|DateTime|File|Integer|Number $content,
         ?string $description,
+        Map $headers,
     ) {
         $this->mediaType = $mediaType;
         $this->content = $content;
         $this->description = $description;
+        $this->headers = $headers;
     }
 
     /**
@@ -45,6 +52,21 @@ final class Definition
         Shape|Sequence|Str|Uuid|Password|Url|Date|DateTime|File|Integer|Number $content,
         ?string $description,
     ): self {
-        return new self($mediaType, $content, $description);
+        return new self($mediaType, $content, $description, Map::of());
+    }
+
+    /**
+     * @psalm-mutation-free
+     *
+     * @param non-empty-string $name
+     */
+    public function withHeader(string $name, Str $schema): self
+    {
+        return new self(
+            $this->mediaType,
+            $this->content,
+            $this->description,
+            ($this->headers)($name, $schema),
+        );
     }
 }
